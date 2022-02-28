be258ba2835012c6b981b02a2868fc5af8f3880b
MahmoudHamdy00 <mahmoudh.morsy@gmail.com>
2022-02-28 14:12:24 +0200

    Configure database

--- ICPC WebSite-Backend/Program.cs
+++ ICPC WebSite-Backend/Program.cs
@@ -1,3 +1,9 @@
+using ICPC_WebSite_Backend.Data;
+using ICPC_WebSite_Backend.Models;
+using ICPC_WebSite_Backend.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddIdentity<User, IdentityRole>(options => options.User.RequireUniqueEmail = true)
+       .AddEntityFrameworkStores<ApplicationDbContext>()
+       .AddDefaultTokenProviders();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
