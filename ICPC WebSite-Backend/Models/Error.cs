70d384819e36df43386920eba42949578b5e2f4e
MahmoudHamdy00 <mahmoudh.morsy@gmail.com>
2022-03-05 01:54:59 +0200

    Add sign up validation

---
+++ ICPC WebSite-Backend/Models/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace ICPC_WebSite_Backend.Models
+{
+    public class Error
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ICPC WebSite-Backend/Models/ValidateResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ICPC_WebSite_Backend.Models
+{
+    public class ValidateResponse
+    {
+        public bool Succeeded { get; set; } = true;
+        public List<Error> Errors { get; set; } = new List<Error>();
+    }
+}
