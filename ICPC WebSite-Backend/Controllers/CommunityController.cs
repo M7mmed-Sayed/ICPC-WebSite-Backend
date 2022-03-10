6539125941e28a909132952f7c38067e55bd5723
MahmoudHamdy00 <mahmoudh.morsy@gmail.com>
2022-03-10 19:38:04 +0200

    implement Community Controller  and rigester community method

---
+++ ICPC WebSite-Backend/Controllers/CommunityController.cs
@@ -0,0 +1,27 @@
+﻿using ICPC_WebSite_Backend.Models.DTO;
+using ICPC_WebSite_Backend.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ICPC_WebSite_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommunityController : ControllerBase
+    {
+        private readonly ICommunityRepository _communityRepository;
+
+        public CommunityController(ICommunityRepository communityRepository) {
+            _communityRepository = communityRepository;
+        }
+        [HttpPost("registerCommunity")]
+        public async Task<IActionResult> Register([FromBody] CommunityDTO community) {
+
+            var result = await _communityRepository.RegisterCommunityAsync(community);
+            if (!result.Succeeded) {
+                return Unauthorized(result.Errors);
+            }
+            return Ok(result);
+        }
+    }
+}
