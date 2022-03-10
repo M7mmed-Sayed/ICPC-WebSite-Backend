20e18784e3f09d1d1fbfe43de31133ec5dbe708e
MahmoudHamdy00 <mahmoudh.morsy@gmail.com>
2022-03-10 19:35:58 +0200

    give IsApproved in community model initial value false

--- ICPC WebSite-Backend/Models/Community.cs
+++ ICPC WebSite-Backend/Models/Community.cs
@@ -1,10 +1,12 @@
-﻿namespace ICPC_WebSite_Backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ICPC_WebSite_Backend.Models
 {
     public class Community
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string About { get; set; }
-        public bool IsApproved { get; set; }
+        public bool IsApproved { get; set; } = false;
     }
 }
