a9e50d641ad61d13a8cc6ed71771007dc2349bf9
MahmoudHamdy00 <mahmoudh.morsy@gmail.com>
2022-03-10 18:45:19 +0200

    Add Community Model and update database

--- ICPC WebSite-Backend/Data/ApplicationDbContext.cs
+++ ICPC WebSite-Backend/Data/ApplicationDbContext.cs
@@ -7,13 +7,12 @@ namespace ICPC_WebSite_Backend.Data
 {
     public class ApplicationDbContext : IdentityDbContext<User>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
 
 
         }
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
+        public DbSet<Community> communities { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder) {
             base.OnModelCreating(builder);
         }
     }
---
+++ ICPC WebSite-Backend/Models/Community.cs
@@ -0,0 +1,10 @@
+﻿namespace ICPC_WebSite_Backend.Models
+{
+    public class Community
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string About { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
