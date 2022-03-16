e3285feed71cb8e0f86c50a669c2acb366657add
M7mmed-Sayed <mohamedsayed1167@gmail.com>
2022-03-16 16:11:07 +0200

    Add test controller to test userRoles functionality

---
+++ ICPC WebSite-Backend/Controllers/TestRoleController.cs
@@ -0,0 +1,61 @@
+﻿using ICPC_WebSite_Backend.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+namespace ICPC_WebSite_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestRoleController : ControllerBase
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public TestRoleController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        [Authorize(Roles = "CommunityLeader")]
+        [HttpGet("CommunityLeader")]
+        public async Task<IActionResult> Index1()
+        {
+            return Ok("CommunityLeader ");
+        }
+        [Authorize(Roles = "HeadOfTraining")]
+        [HttpGet("HeadOfTraining")]
+        public async Task<IActionResult> Index2()
+        {
+            return Ok("HeadOfTraining ");
+        }
+        [Authorize(Roles = "TrainingManager")]
+        [HttpGet("TrainingManager")]
+        public async Task<IActionResult> Index3()
+        {
+            return Ok("TrainingManager ");
+        }
+        [Authorize(Roles = "Mentor")]
+        [HttpGet("Mentor")]
+        public async Task<IActionResult> Index4()
+        {
+            return Ok("Mentor ");
+        }
+        [Authorize(Roles = "Trainee")]
+        [HttpGet("Trainee")]
+        public async Task<IActionResult> Index5()
+        {
+            return Ok("Trainee ");
+        }
+        [HttpPost("Create-Role")]
+        public async Task<IActionResult> CreateRole(Models.UserRole userRole)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var result = await _accountRepository.AddRoleAsync(userRole);
+
+            if (!string.IsNullOrEmpty(result))
+                return BadRequest(result);
+
+            return Ok(userRole);
+        }
+    }
+}
