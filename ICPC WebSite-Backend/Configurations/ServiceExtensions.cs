0597e4a150ba323024f988cbe72b45bf6a23b90e
MahmoudHamdy00 <mahmoudh.morsy@gmail.com>
2022-03-19 22:39:56 +0200

    Seperate configuration in ServiceExtensions
    Related Work Items: #64

---
+++ ICPC WebSite-Backend/Configurations/ServiceExtensions.cs
@@ -0,0 +1,51 @@
+﻿using ICPC_WebSite_Backend.Data;
+using ICPC_WebSite_Backend.Models;
+using ICPC_WebSite_Backend.Repository;
+using ICPC_WebSite_Backend.Utility;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace ICPC_WebSite_Backend.Configurations
+{
+    public static class ServiceExtensions
+    {
+        // public static void RegisterRepos(this IServiceCollection Services) {
+        public static void RegisterRepos(this IServiceCollection Services, IConfiguration Configuration) {
+            Services.AddTransient<IAccountRepository, AccountRepository>();
+            Services.AddTransient<ICommunityRepository, CommunityRepository>();
+            var myEmail = Configuration["email"];
+            var myPassword = Configuration["emailpassword"];
+            var SMTPServerAddress = Configuration["SMTPServerAddress"];
+            var mailSubmissionPort = Convert.ToInt32(Configuration["mailSubmissionPort"]);
+            Services.AddTransient<IEmailSender, EmailSender>(op => new EmailSender(myEmail, myPassword, SMTPServerAddress, mailSubmissionPort));
+
+        }
+        public static void RegisterAuth(this IServiceCollection Services, IConfiguration Configuration) {
+            Services.AddAuthentication(option => {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(option => {
+                option.SaveToken = true;
+                option.RequireHttpsMetadata = false;
+                option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters() {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = Configuration["JWT:ValidAudience"],
+                    ValidIssuer = Configuration["JWT:ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+                };
+            });
+        }
+        public static void ConfigureDatabase(this IServiceCollection Services, IConfiguration Configuration) {
+            Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+            Services.AddIdentity<User, IdentityRole>(options => options.User.RequireUniqueEmail = true)
+                   .AddEntityFrameworkStores<ApplicationDbContext>()
+                   .AddDefaultTokenProviders();
+
+        }
+    }
+}
--- ICPC WebSite-Backend/Program.cs
+++ ICPC WebSite-Backend/Program.cs
@@ -1,12 +1,4 @@
-using ICPC_WebSite_Backend.Data;
-using ICPC_WebSite_Backend.Models;
-using ICPC_WebSite_Backend.Repository;
-using ICPC_WebSite_Backend.Utility;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using ICPC_WebSite_Backend.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,34 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
-builder.Services.AddIdentity<User, IdentityRole>(options => options.User.RequireUniqueEmail = true)
-       .AddEntityFrameworkStores<ApplicationDbContext>()
-       .AddDefaultTokenProviders();
-builder.Services.AddTransient<IAccountRepository, AccountRepository>();
-builder.Services.AddTransient<ICommunityRepository, CommunityRepository>();
 
-builder.Services.AddAuthentication(option => {
-    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-                .AddJwtBearer(option => {
-                    option.SaveToken = true;
-                    option.RequireHttpsMetadata = false;
-                    option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters() {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidAudience = builder.Configuration["JWT:ValidAudience"],
-                        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
-                    };
-                });
-var myEmail = builder.Configuration["email"];
-var myPassword = builder.Configuration["emailpassword"];
-var SMTPServerAddress = builder.Configuration["SMTPServerAddress"];
-var mailSubmissionPort = Convert.ToInt32(builder.Configuration["mailSubmissionPort"]);
-builder.Services.AddTransient<IEmailSender, EmailSender>(op => new EmailSender(myEmail, myPassword, SMTPServerAddress, mailSubmissionPort));
+builder.Services.ConfigureDatabase(builder.Configuration);
+builder.Services.RegisterRepos(builder.Configuration);
+builder.Services.RegisterAuth(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
