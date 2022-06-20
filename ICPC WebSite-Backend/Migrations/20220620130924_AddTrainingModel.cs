5c4267722ac9113c2fb44472977df6247e9deda6
Mahmoud-Abd-AlKareem <Mahmud.18321746@compit.aun.edu.eg>
2022-06-20 15:13:31 +0200

    Create Training Model

---
+++ ICPC WebSite-Backend/Migrations/20220620130924_AddTrainingModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ICPC_WebSite_Backend.Migrations
+{
+    public partial class AddTrainingModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Training",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Created_At = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Upated_At = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Level = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsPublic = table.Column<bool>(type: "bit", nullable: false),
+                    Community_Id = table.Column<int>(type: "int", nullable: false),
+                    CommunityId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Training", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Training_communities_CommunityId",
+                        column: x => x.CommunityId,
+                        principalTable: "communities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Training_CommunityId",
+                table: "Training",
+                column: "CommunityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Training");
+        }
+    }
+}
