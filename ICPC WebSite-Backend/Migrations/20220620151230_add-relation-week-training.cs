7d6a4b48252c8d4d90ab1bf1a73a97f8f8fb66ad
M7mmed-Sayed <mohamed.sayed@compit.aun.edu.eg>
2022-06-20 17:52:48 +0200

    add-training realation with weeks

---
+++ ICPC WebSite-Backend/Migrations/20220620151230_add-relation-week-training.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ICPC_WebSite_Backend.Migrations
+{
+    public partial class addrelationweektraining : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "weeks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsTemplate = table.Column<bool>(type: "bit", nullable: false),
+                    Created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Updated_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Training_Id = table.Column<int>(type: "int", nullable: false),
+                    TrainingId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_weeks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_weeks_Training_TrainingId",
+                        column: x => x.TrainingId,
+                        principalTable: "Training",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "matirials",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    URL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Updated_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    weekId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_matirials", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_matirials_weeks_weekId",
+                        column: x => x.weekId,
+                        principalTable: "weeks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_matirials_weekId",
+                table: "matirials",
+                column: "weekId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_weeks_Name",
+                table: "weeks",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_weeks_TrainingId",
+                table: "weeks",
+                column: "TrainingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "matirials");
+
+            migrationBuilder.DropTable(
+                name: "weeks");
+        }
+    }
+}
